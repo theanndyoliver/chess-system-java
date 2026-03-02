f9ccc9f068e57bff48628b960061b4fa4f0d832b
theanndyoliver <adm-loren@hotmail.com>
2026-03-02 16:15:59 -0400

    First class Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,12 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		
+		Position pos = new Position(2,6);
+		System.out.println(pos);
 	}
 
 }
